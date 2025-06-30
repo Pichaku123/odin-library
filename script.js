26143782a32e38bb572026a860d3a0f12fdb16b6
Pichaku123 <pribisht785@gmail.com>
2025-06-30 13:45:56 +0530

    can edit html to show cards with details of books

--- script.js
+++ script.js
@@ -13,7 +13,30 @@ function addToLib(author, title, pages, read){
     library.push(book);
 }
 
+function displayBook(library, card, i){
+
+        card.innerHTML+=`Book No. ${i+1}<br>
+        Author- ${library[i].author}<br>
+        Title- ${library[i].title}<br>
+        Pages- ${library[i].pages}<br>
+        Read or not- ${library[i].read}<br><br>`;
+    
+}
 
 addToLib("me", "mybook", 200, true);
-console.log(library);
-console.log(library[0].title);
+addToLib("someone else", "theirbook", 500, false);
+addToLib("some third person", "whatever", 10, true);
+addToLib("balwant", "tiger zinda hai", 44, true);
+addToLib("balwant", "tiger kyu nahi marega", 100, true);
+
+const container=document.querySelector(".container");
+const noOfCards=library.length;
+
+library.reduce((bookNo, book) => {
+    const card=document.createElement("div");
+    card.classList.add("card");
+    displayBook(library, card, bookNo);
+    container.appendChild(card);
+    return ++bookNo;
+}, 0);
+
